bea8a3dd6a41ecbd777e743347fc788407cbb713
Ervin Racz <ervin.racz@protonmail.com>
2023-06-03 21:14:33 +0300

    refine tmux opening

--- .dotfiles/target-home/.config/nvim/init.lua
+++ .dotfiles/target-home/.config/nvim/init.lua
@@ -1,1 +1,17 @@
 require("core")
+
+function update_tmux_window_name()
+    -- local file_path = vim.fn.expand('%:p')
+    -- local root_path = vim.fn.getcwd()
+    -- local relative_path = vim.fn.fnamemodify(file_path, ':~:.')
+    local file_name = vim.fn.expand('%:t')
+    -- os.execute('tmux rename-window "' .. relative_path .. '/' .. file_name .. '"')
+    os.execute('tmux rename-window "' .. file_name .. '"')
+end
+
+vim.api.nvim_exec([[
+    augroup TmuxWindowName
+        autocmd!
+        autocmd BufEnter * lua update_tmux_window_name()
+    augroup END
+]], true)
