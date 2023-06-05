6d0a43e8d6f39e7cc9b8fadbaaec2bd98056d425
Ervin Racz <ervin.racz@protonmail.com>
2023-06-05 21:49:22 +0300

    Configure lsp

--- .dotfiles/target-home/.config/nvim/after/plugin/lsp.lua
+++ .dotfiles/target-home/.config/nvim/after/plugin/lsp.lua
@@ -1,43 +1,58 @@
 local lsp = require('lsp-zero')
 
+-- Custom file types
+vim.filetype.add({
+    extension = {
+        eslintrc = "json",
+        mdx = "markdown",
+        prettierrc = "json",
+        mjml = "html",
+    },
+    pattern = {
+        [".*%.env.*"] = "sh",
+    },
+})
+
 lsp.preset('recommended')
 
 lsp.ensure_installed({
-	'tsserver',
-	'eslint',
+    'tsserver',
+    'eslint',
+    'tailwindcss',
+    'html',
+    'cssls',
+    'bashls',
 })
 
--- local cmp = require('cmp')
--- local cmp_select = {behavior = cmp.SelectBehavior.Select}
--- local cmp_mappings = lsp.defaults.cmp_mappings({
---   ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
---   ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
---   ['<C-y>'] = cmp.mapping.confirm({ select = true }),
---   ["<C-Space>"] = cmp.mapping.complete(),
--- })
--- 
--- cmp_mappings['<Tab>'] = nil
--- cmp_mappings['<S-Tab>'] = nil
--- 
--- lsp.setup_nvim_cmp({
---   mapping = cmp_mappings
--- })
---
+local cmp = require('cmp')
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
+local cmp_mappings = lsp.defaults.cmp_mappings({
+    ['<C-k>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<C-j>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<Tab>'] = cmp.mapping.confirm({ select = true }),
+    ["<C-Space>"] = cmp.mapping.complete(),
+})
+
+cmp_mappings['<S-Tab>'] = nil
+
+lsp.setup_nvim_cmp({
+    mapping = cmp_mappings
+})
 
 lsp.on_attach(function(client, bufnr)
-  local opts = {buffer = bufnr, remap = false}
+    local opts = { buffer = bufnr, remap = false }
 
-  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-  -- vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-  -- vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-  -- vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-  vim.keymap.set("n", "<a-e>", function() vim.diagnostic.goto_next() end, opts)
-  vim.keymap.set("n", "<a-E>", function() vim.diagnostic.goto_prev() end, opts)
-  vim.keymap.set("n", "<a-cr>", function() vim.lsp.buf.code_action() end, opts)
-  vim.keymap.set("i", "<a-cr>", function() vim.lsp.buf.code_action() end, opts)
-  -- vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-  vim.keymap.set("n", "<leader>r", function() vim.lsp.buf.rename() end, opts)
-  -- vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    -- vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+    -- vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+    -- vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+    vim.keymap.set("n", "<a-e>", function() vim.diagnostic.goto_next() end, opts)
+    vim.keymap.set("n", "<a-E>", function() vim.diagnostic.goto_prev() end, opts)
+    vim.keymap.set("n", "<a-cr>", function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set("i", "<a-cr>", function() vim.lsp.buf.code_action() end, opts)
+    -- vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+    vim.keymap.set("n", "<leader>r", function() vim.lsp.buf.rename() end, opts)
+    -- vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 end)
 
 lsp.setup()
