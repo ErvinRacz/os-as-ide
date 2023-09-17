c5dd1b439a37a6a5e09df3b98ab9757ca00387eb
Ervin Racz <ervin.racz@protonmail.com>
2023-09-17 14:34:39 +0300

    feat add cool git commit autocompletion

--- .dotfiles/target-home/.config/nvim/after/plugin/fugitive.lua
+++ .dotfiles/target-home/.config/nvim/after/plugin/fugitive.lua
@@ -32,10 +32,72 @@ autocmd("BufWinEnter", {
     end,
 })
 
+local cmpRegistered = false
+
 autocmd("FileType", {
     group = Core_Fugitive,
     pattern = { "gitcommit" },
     callback = function()
+        vim.cmd([[cnoreabbrev <buffer> q q!]]) -- apply only to this current buffer
         vim.cmd("startinsert")
+
+        if cmpRegistered then
+            return
+        end
+
+        cmpRegistered = true
+
+        local has_cmp, cmp = pcall(require, "cmp")
+        if not has_cmp then
+            return
+        end
+        -- list of common words from commit messages
+        local words = {
+            "fix",
+            "feat",
+            "docs",
+            "style",
+            "refactor",
+            "perf",
+            "test",
+            "chore",
+            "revert",
+        }
+        -- get task number read from current branch if any with the regular expression:
+        local task = vim.fn.systemlist("git branch --show-current")[1]:match("([A-Z]{2,8}-\\d+).*$")
+
+        -- add the two tables together
+        local items = vim.tbl_flatten({ words, task })
+
+        local source = {}
+
+        source.new = function()
+            return setmetatable({}, { __index = source })
+        end
+
+        source.get_trigger_characters = function()
+            return { " ", "\n" }
+        end
+
+        source.complete = function(self, request, callback)
+            local autocompleteItems = {}
+
+            for _, word in ipairs(items) do
+                table.insert(autocompleteItems, {
+                    label = word,
+                    kind = cmp.lsp.CompletionItemKind.Text,
+                    filter_text = request.context.cursor_before_line,
+                })
+            end
+
+            callback({ items = autocompleteItems, isIncomplete = true })
+        end
+
+        cmp.register_source("git_commits_common_words", source.new())
+        cmp.setup.filetype("gitcommit", {
+            sources = cmp.config.sources({
+                { name = "git_commits_common_words" },
+            })
+        })
     end,
 })
